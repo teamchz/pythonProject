d6eddbf4f853253d0868d157958308f260574a9b
teamchz <teamchz@gmail.com>
2020-10-26 13:44:20 +0700

    fixed indentation

--- CSS2_3.py
+++ CSS2_3.py
@@ -5,14 +5,14 @@ print()
 
 # Create row Period: 1 - 10 %
 row = [x for x in range(1, 11)]
-print("Period\t", end="")
+print("{:<10}".format("Period"), end="")
 for i in row:
-    print(str(i) + "%", end="\t")
+    print("{:<10}".format(str(i)+"%"), end="")
 print()
 
 # Create each column and calculate
 for i in range(n):
-    print(i+1, end="\t")
+    print("{:<10}".format(i+1), end="")
     for j in range(10):
-        print("%4.3f" % (1 + (j+1) * 0.01)**(i+1), end="\t")
+        print("{:<10}".format("%4.3f" % (1 + (j+1) * 0.01)**(i+1)), end="")
     print()
