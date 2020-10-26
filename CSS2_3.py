e5b70457461a97df9ad05aff5587bc4bbd56a5f7
Pisanu <peat2512@hotmail.com>
2020-10-26 12:26:44 +0700

    Fix tab

--- CSS2_3.py
+++ CSS2_3.py
@@ -5,14 +5,14 @@ print()
 
 # Create row Period: 1 - 10 %
 row = [x for x in range(1, 11)]
-print("Period\t\t", end="")
+print("Period\t", end="")
 for i in row:
-    print(str(i) + "%", end="\t\t")
+    print(str(i) + "%", end="\t")
 print()
 
 # Create each column and calculate
 for i in range(n):
-    print(i+1, end="\t\t\t")
+    print(i+1, end="\t")
     for j in range(10):
         print("%4.3f" % (1 + (j+1) * 0.01)**(i+1), end="\t")
     print()
